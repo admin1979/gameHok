d474013b3203239284d5d87cc1df163ba0b6cda3
admin1979 <oleg bachinskiy@gmail.com>
2021-07-07 17:13:32 +0300

    chapter 9

---
+++ Game9/Assets/Scripts/DeviceTrigger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+public class DeviceTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject[] targets;
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject target in targets)
+        {
+            target.SendMessage("Activate");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject target in targets)
+        {
+            target.SendMessage("Deactivate");
+        }
+    }
+}
